0ead8730c78a948e37699e0235bc61f52cb10fac
Aragami <tim.velling@hotmail.de>
2023-01-11 20:54:57 +0100

    ModUpdater UI in main menu

--- Mods/Workshopupdater/WorkshopupdaterMain.cs
+++ Mods/Workshopupdater/WorkshopupdaterMain.cs
@@ -7,9 +7,33 @@ using Steamworks.Ugc;
 using System.Collections.Generic;
 using System.Threading.Tasks;
 using HarmonyLib;
+using System;
+using KitchenLib;
 
 namespace Workshopupdater
 {
+    [HarmonyPatch(typeof(StartMainMenu), nameof(StartMainMenu.Setup))]
+    public class StartMainMenu_Patch
+    {
+        [HarmonyPostfix]
+        public static void Postfix(StartMainMenu __instance)
+        {
+            MethodInfo addButton = __instance.GetType().GetMethod("AddButton", BindingFlags.NonPublic | BindingFlags.Instance);
+            addButton.Invoke(__instance, new object[] { WorkshopupdaterMain.HasModUpdate ? "UPDATES AVAILABLE" : "All mods up-to-date", (Action<int>)(i => WorkshopupdaterMain.UpdateAllMods()), 0, 1f, 0.2f });
+        }
+    }
+
+    [HarmonyPatch(typeof(RevisedMainMenu), nameof(RevisedMainMenu.Setup))]
+    public class RevisedMainMenu_Patch
+    {
+        [HarmonyPostfix]
+        public static void Postfix(RevisedMainMenu __instance)
+        {
+            MethodInfo addButton = __instance.GetType().GetMethod("AddButton", BindingFlags.NonPublic | BindingFlags.Instance);
+            addButton.Invoke(__instance, new object[] { WorkshopupdaterMain.HasModUpdate ? "UPDATES AVAILABLE" : "All mods up-to-date", (Action<int>)(i => WorkshopupdaterMain.UpdateAllMods()), 0, 1f, 0.2f });
+        }
+    }
+
     public class WorkshopupdaterMain : GenericSystemBase, IModSystem, IModInitializer
     {
         // guid must be unique and is recommended to be in reverse domain name notation
@@ -29,11 +53,11 @@ namespace Workshopupdater
             base.OnStartRunning();
         }
 
+        private static readonly Harmony m_harmony = new Harmony("aragami.plateup.mods.workshopupdater");
+
         protected override void Initialise()
         {
             base.Initialise();
-            // For log file output so the official plateup support staff can identify if/which a mod is being used
-            LogError($"{MOD_GUID} v{MOD_VERSION} in use!");
             //SteamUGC.Download(2916090048, true);
         }
 
@@ -42,9 +66,32 @@ namespace Workshopupdater
 
         }
 
-        public static async void UpdateAllMods()
+        public static List<Item> updateMods = new List<Item>();
+
+        public static bool HasModUpdate => updateMods.Count > 0;
+
+        public static void UpdateAllMods()
         {
-            List<Item> updateMods = new List<Item>();
+            // TODO: unload all mods excpet this one - nvm
+            if (updateMods.Count > 0)
+            {
+                foreach (Item updateMod in updateMods)
+                {
+                    LogInfo("Updating Mod: " + updateMod.Id + " " + updateMod.Title);
+                    updateMod.Download(false);
+                }
+                // Close game immediatly after forcing mod updates
+                LogInfo("Exiting game for workshop updates");
+                Session.SoftExit();
+            }
+            else
+            {
+                LogWarning("Attempting to update mods, but there are not updates!");
+            }
+        }
+
+        public static async void RetriveModUpdates()
+        {
             Query subedItemsQuery = Query.ItemsReadyToUse.WhereUserSubscribed(SteamClient.SteamId.AccountId);
             ResultPage? ugcResult = await subedItemsQuery.GetPageAsync(1); // TODO: multiple pages? - how many?
             if (ugcResult != null)
@@ -58,19 +105,6 @@ namespace Workshopupdater
                     }
                 }
             }
-            // TODO: unload all mods excpet this one - nvm
-            if (updateMods.Count > 0)
-            {
-                foreach (Item updateMod in updateMods)
-                {
-                    LogInfo("Updating Mod: " + updateMod.Id + " " + updateMod.Title);
-                    updateMod.Download(false);
-                }
-                // Close game immediatly after forcing mod updates
-                LogInfo("Exiting game for workshop updates");
-                Session.SoftExit();
-
-            }
         }
 
         #region Logging
@@ -82,11 +116,44 @@ namespace Workshopupdater
         public static void LogWarning(object _log) { LogWarning(_log.ToString()); }
         public static void LogError(object _log) { LogError(_log.ToString()); }
 
+        public static void PatchHarmony()
+        {
+            List<Mod> mods = ModPreload.Mods;
+            bool isKitchenLibInstalled = false;
+            foreach (Mod mod in mods)
+            {
+                // KitchenLib workshop id or name
+                if (mod.Name == "2898069883" || mod.Name == "KitchenLib")
+                {
+                    isKitchenLibInstalled = true;
+                    break;
+                }
+            }
+
+            MethodInfo original;
+            MethodInfo postfix;
+
+            if (isKitchenLibInstalled)
+            {
+                original = typeof(RevisedMainMenu).GetMethod(nameof(RevisedMainMenu.Setup));
+                postfix = typeof(RevisedMainMenu_Patch).GetMethod(nameof(RevisedMainMenu_Patch.Postfix));
+            }
+            else
+            {
+                original = typeof(StartMainMenu).GetMethod(nameof(StartMainMenu.Setup));
+                postfix = typeof(StartMainMenu_Patch).GetMethod(nameof(StartMainMenu_Patch.Postfix));
+            }
+            m_harmony.Patch(original, postfix: new HarmonyMethod(postfix));
+        }
+
         public void PostActivate(Mod mod)
         {
             //TODO: Patch main menu
+            LogError($"{MOD_GUID} v{MOD_VERSION} in use!");
             LogError("Patch Main menu here");
-            UpdateAllMods();
+            RetriveModUpdates();
+            // Patch after initializing mods
+            PatchHarmony();
         }
 
         public void PreInject() { }
