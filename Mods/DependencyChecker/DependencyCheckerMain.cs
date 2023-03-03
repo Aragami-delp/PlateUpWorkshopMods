5a8ba9360ab894cadd5386cd5b3f294296f175fc
Aragami <tim.velling@hotmail.de>
2023-03-03 15:58:17 +0100

    DependencyChecker first version

---
+++ Mods/DependencyChecker/DependencyCheckerMain.cs
@@ -0,0 +1,124 @@
+﻿using Kitchen;
+using KitchenMods;
+using UnityEngine;
+using System.Collections.Generic;
+using Steamworks.Data;
+using System.Threading.Tasks;
+using Steamworks.Ugc;
+using System.Linq;
+using System.Windows.Forms;
+
+// Namespace should have "Kitchen" in the beginning
+namespace KitchenDependencyChecker
+{
+    public class DependencyCheckerMain : GenericSystemBase, IModSystem, IModInitializer
+    {
+        // GUID must be unique and is recommended to be in reverse domain name notation
+        // Mod Name is displayed to the player and listed in the mods menu
+        // Mod Version must follow semver notation e.g. "1.2.3"
+        public const string MOD_GUID = "aragami.plateup.mods.dependencyChecker";
+        public const string MOD_NAME = "Dependency Checker";
+        public const string MOD_VERSION = "0.1.0";
+        public const string MOD_AUTHOR = "Aragami";
+        public const string MOD_GAMEVERSION = ">=1.1.4";
+        // Game version this mod is designed for in semver
+        // e.g. ">=1.1.3" current and all future
+        // e.g. ">=1.1.3 <=1.2.3" for all from/until
+
+        // Boolean constant whose value depends on whether you built with DEBUG or RELEASE mode, useful for testing
+#if DEBUG
+        public const bool DEBUG_MODE = true;
+#else
+        public const bool DEBUG_MODE = false;
+#endif
+
+        public void PostActivate(Mod mod)
+        {
+            LogWarning($"{MOD_GUID} v{MOD_VERSION} in use!");
+
+            HashSet<PublishedFileId> lastItems = Helper.LoadLastModList();
+            List<Item> currentItems = Helper.GetInstalledModItems();
+            //List<Item> newItems = currentItems.Where(item => !lastItems.Contains(item.Id)).ToList(); // Should get all new items // Check all for dependency change whenever a single mod is added/removed
+
+            if (!lastItems.ToHashSet().SetEquals(currentItems.Select(x => x.Id)))
+            {
+                HashSet<PublishedFileId> deps = Task.Run(() => Helper.GetAllModDependencies(currentItems)).GetAwaiter().GetResult();
+                if (deps.Count > 0)
+                {
+                    foreach (PublishedFileId depId in deps.ToHashSet())
+                    {
+                        foreach (Item item in currentItems)
+                        {
+                            if (item.Id == depId)
+                            {
+                                deps.Remove(depId);
+                            }
+                        }
+                    }
+                    List<Item> dependencyItems = Task.Run(() => Helper.GetModItems(deps)).GetAwaiter().GetResult();
+
+                    if (dependencyItems.Count > 0)
+                        ShowDependencyDialog(dependencyItems);
+                    else
+                        Helper.SaveCurrentModList(currentItems);
+                }
+                else
+                    Helper.SaveCurrentModList(currentItems);
+            }
+            else
+                Helper.SaveCurrentModList(currentItems);
+        }
+
+        private void ShowDependencyDialog(List<Item> _missingItems)
+        {
+            string messageStart = "Some of your mods are missing dependencies in order to work properly. Missing Mods:";
+            string messageBody = string.Empty;
+            foreach (Item missingItem in _missingItems)
+            {
+                messageBody += "\n" + missingItem.Title;
+            }
+            string messageEnd = "\n\nPress \"Yes\" to close the game and install these automatically.\nPress \"No\" to close the game.\nPress \"Cancel\" to start anyways.";
+            bool startGame = false;
+            DialogResult result = MessageBox.Show(messageStart + messageBody + messageEnd, "DependencyChecker", MessageBoxButtons.YesNoCancel, MessageBoxIcon.Error);
+            if (result == DialogResult.Yes)
+            {
+                Helper.SaveCurrentModList();
+                Task.Run(async () => await Helper.InstallItems(_missingItems)).GetAwaiter().GetResult();
+                startGame = false;
+            }
+            else if (result == DialogResult.No)
+            {
+                startGame = false;
+            }
+            else if (result == DialogResult.Cancel)
+            {
+                startGame = true;
+            }
+            if (!startGame) { Session.SoftExit(); }
+        }
+
+        public void PreInject()
+        {
+            
+        }
+
+        public void PostInject()
+        {
+            
+        }
+
+        protected override void OnUpdate()
+        {
+            
+        }
+
+        #region Logging
+        public static void LogInfo(string _log) { Debug.Log($"[{MOD_NAME}] " + _log); }
+        public static void LogWarning(string _log) { Debug.LogWarning($"[{MOD_NAME}] " + _log); }
+        public static void LogError(string _log) { Debug.LogError($"[{MOD_NAME}] " + _log); }
+        public static void LogInfo(object _log) { LogInfo(_log.ToString()); }
+        public static void LogWarning(object _log) { LogWarning(_log.ToString()); }
+        public static void LogError(object _log) { LogError(_log.ToString()); }
+        #endregion
+    }
+}
---
+++ Mods/DependencyChecker/Helper.cs
@@ -0,0 +1,252 @@
+﻿using Newtonsoft.Json;
+using Steamworks.Ugc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+using System.IO;
+using System.Net.Http;
+using System.Web;
+using System.Text.RegularExpressions;
+using Steamworks.Data;
+using Microsoft.Win32;
+
+namespace KitchenDependencyChecker
+{
+    public static class Helper
+    {
+        private static readonly Regex MODDEPENDENCIESLINE = new("<a href=\"https:\\/\\/steamcommunity.com\\/workshop\\/filedetails\\/\\?id=\\d+\" target=\"_blank\">", RegexOptions.IgnoreCase);
+        private static readonly string MODCACHEFOLDER = Application.persistentDataPath + "/DependencyChecker";
+        private static readonly string MODCACHEFILE = "LastMods.json";
+
+        public static string MODCACHEFOLDERANDFILE => MODCACHEFOLDER + "/" + MODCACHEFILE;
+        private static List<Item> m_installedItems = null;
+
+
+        public static List<Item> GetInstalledModItems()
+        {
+            if (m_installedItems != null)
+            {
+                return m_installedItems;
+            }
+
+            string steamPath = String.Empty;
+            try
+            {
+                using RegistryKey key = Registry.LocalMachine.OpenSubKey("Software\\Wow6432Node\\Valve\\Steam");
+                if (key != null)
+                {
+                    object o = key?.GetValue("InstallPath") as string;
+                    if (o != null)
+                    {
+                        steamPath = o.ToString();
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                DependencyCheckerMain.LogError("Error reading steam install path");
+            }
+
+            if (string.IsNullOrEmpty(steamPath))
+                return Task.Run(() => GetSubscribedModItems()).GetAwaiter().GetResult();
+
+
+            string[] libraryfoldersVDF = File.ReadAllLines(steamPath + "\\steamapps\\libraryfolders.vdf");
+            string currentPath = string.Empty;
+            foreach (string line in libraryfoldersVDF)
+            {
+                if (line.Contains("\"path\""))
+                {
+                    string[] parts = line.Split(new char[] {'"'}, StringSplitOptions.RemoveEmptyEntries);
+                    currentPath = parts[3].Replace("\\\\", "\\");
+                }
+                if (line.Contains("\"1599600\""))
+                {
+                    break;
+                }
+            }
+            if (string.IsNullOrEmpty(currentPath))
+            {
+                DependencyCheckerMain.LogError("No valid library path found");
+            }
+            currentPath = currentPath + "\\steamapps\\workshop\\content\\1599600";
+            string[] foundItemStringIds = Directory.GetDirectories(currentPath);
+            HashSet<PublishedFileId> foundItemIds = new();
+            foreach (string foundItemStringId in foundItemStringIds)
+            {
+                if (ulong.TryParse(Path.GetFileName(foundItemStringId), out ulong _result))
+                {
+                    foundItemIds.Add(_result);
+                }
+                else
+                {
+                    DependencyCheckerMain.LogError("Can't read workshop item id");
+                }
+            }
+            m_installedItems = Task.Run(() => GetModItems(foundItemIds)).GetAwaiter().GetResult();
+            return m_installedItems;
+        }
+
+        public static async Task<List<Item>> GetSubscribedModItems()
+        {
+            List<Item> items = new();
+            int page_number = 1;
+            int result_count = 0;
+            ResultPage value;
+            do
+            {
+                ResultPage? page = await Query.Items.WhereUserSubscribed().GetPageAsync(page_number);
+                if (!page.HasValue)
+                {
+                    break;
+                }
+
+                value = page.Value;
+                items.AddRange(value.Entries);
+                result_count += value.ResultCount;
+                page_number++;
+            }
+            while (value.ResultCount != 0 && result_count < value.TotalCount);
+
+            return items;
+        }
+
+        public static async Task<List<Item>> GetModItems(HashSet<PublishedFileId> _ids)
+        {
+            List<Item> items = new();
+            int page_number = 1;
+            int result_count = 0;
+            ResultPage value;
+            do
+            {
+                ResultPage? page = await Query.Items.WithFileId(_ids.ToArray()).GetPageAsync(page_number);
+                if (!page.HasValue)
+                {
+                    break;
+                }
+
+                value = page.Value;
+                items.AddRange(value.Entries);
+                result_count += value.ResultCount;
+                page_number++;
+            }
+            while (value.ResultCount != 0 && result_count < value.TotalCount);
+
+            return items;
+        }
+
+        public static void SaveCurrentModList(List<Item> _items = null)
+        {
+            _items ??= Task.Run(() => GetSubscribedModItems()).GetAwaiter().GetResult();
+
+            HashSet<long> itemIDs = new();
+            foreach (Item item in _items)
+            {
+                itemIDs.Add((long)item.Id.Value);
+            }
+            SaveCurrentModList(itemIDs);
+        }
+
+        private static void SaveCurrentModList(HashSet<long> _items)
+        {
+            if (!Directory.Exists(MODCACHEFOLDER))
+                Directory.CreateDirectory(MODCACHEFOLDER);
+            string currentJson = JsonConvert.SerializeObject(_items, Formatting.Indented);
+            File.WriteAllText(MODCACHEFOLDERANDFILE, currentJson);
+        }
+
+        public static HashSet<PublishedFileId> LoadLastModList()
+        {
+            if (!Directory.Exists(MODCACHEFOLDER))
+                Directory.CreateDirectory(MODCACHEFOLDER);
+
+            HashSet<long> itemIDs = new();
+            try
+            {
+                try
+                {
+                    string text = System.IO.File.ReadAllText(MODCACHEFOLDERANDFILE);
+                    itemIDs = JsonConvert.DeserializeObject<HashSet<long>>(text);
+                }
+                catch (FileNotFoundException _fileEx)
+                {
+                    DependencyCheckerMain.LogWarning("No LastMods.json file to load, probably started for the first time.");
+                }
+            }
+            catch (Exception _e)
+            {
+                DependencyCheckerMain.LogError(_e.Message);
+            }
+            HashSet<PublishedFileId> convertedIDs = new();
+            foreach (long itemId in itemIDs)
+            {
+                PublishedFileId tmp = (ulong)itemId;
+                convertedIDs.Add(tmp);
+            }
+            return convertedIDs;
+        }
+
+        public static HashSet<PublishedFileId> GetAllModDependencies(List<Item> _items)
+        {
+            HashSet<PublishedFileId> depItems = new();
+            foreach (Item item in _items)
+            {
+                depItems.UnionWith(Task.Run(() => GetSingleModDependencies(item)).GetAwaiter().GetResult());
+            }
+            return depItems;
+        }
+
+        private static async Task<HashSet<PublishedFileId>> GetSingleModDependencies(Item _item)
+        {
+            // Originally made by ZekNikZ for KitchenLib to check for Change Notes
+            HttpClient client = new();
+            using HttpResponseMessage response = await client.GetAsync(_item.Url);
+            using HttpContent content = response.Content;
+            string pageContent = await content.ReadAsStringAsync();
+            List<string> extractedContent = MODDEPENDENCIESLINE.Matches(pageContent).Cast<Match>().Select(match => match.Value).ToList();
+            HashSet<PublishedFileId> retVal = new();
+            foreach (string match in extractedContent)
+            {
+                string resultString = Regex.Match(match, @"\d+").Value;
+                if (ulong.TryParse(resultString, out ulong _ret))
+                {
+                    PublishedFileId tmp = _ret;
+                    retVal.Add(tmp);
+                }
+                else
+                {
+                    DependencyCheckerMain.LogWarning("Cannot Parse \"" + resultString + "\"");
+                }
+            }
+            return retVal;
+        }
+
+        //public static List<Item> GetItems(HashSet<PublishedFileId> _itemIDs)
+        //{
+        //    List<Item> installedMods = Task.Run(() => GetSubscribedModItems()).GetAwaiter().GetResult();
+
+        //    foreach (Item item in installedMods)
+        //    {
+        //        if (_itemIDs.Contains(item.Id))
+        //        {
+        //            _itemIDs.Remove(item.Id);
+        //        }
+        //    }
+
+        //    return Task.Run(() => GetModItems(_itemIDs)).GetAwaiter().GetResult();
+        //}
+
+        public static async Task<int> InstallItems(List<Item> _items)
+        {
+            foreach (Item itemsToInstall in _items)
+            {
+                await itemsToInstall.Subscribe();
+                itemsToInstall.Download(true);
+            }
+            return 1;
+        }
+    }
+}
