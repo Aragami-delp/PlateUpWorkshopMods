2a4bd98181b1b3204bcb545928ef8e75e4a85363
Aragami_ <Aragami@aragami.com>
2023-09-25 15:09:20 +0200

    BDRP Test

---
+++ Mods/BetterDiscordRichPresence/BaseMod.cs
@@ -0,0 +1,56 @@
+﻿using HarmonyLib;
+using Kitchen;
+using KitchenMods;
+using System.Reflection;
+using UnityEngine;
+
+// Namespace should have "Kitchen" in the beginning
+namespace KitchenBetterDiscordRichPresence
+{
+    public class BaseMod : GenericSystemBase, IModSystem
+    {
+        // GUID must be unique and is recommended to be in reverse domain name notation
+        // Mod Name is displayed to the player and listed in the mods menu
+        // Mod Version must follow semver notation e.g. "1.2.3"
+        public const string MOD_GUID = "aragami.plateup.mods.betterdiscordRichPresence";
+        public const string MOD_NAME = "Better Discord Rich Presence";
+        public const string MOD_VERSION = "0.1.0";
+        public const string MOD_AUTHOR = "Aragami";
+        public const string MOD_GAMEVERSION = ">=1.1.7";
+        // Game version this mod is designed for in semver
+        // e.g. ">=1.1.3" current and all future
+        // e.g. ">=1.1.3 <=1.2.3" for all from/until
+
+        // Boolean constant whose value depends on whether you built with DEBUG or RELEASE mode, useful for testing
+#if DEBUG
+        public const bool DEBUG_MODE = true;
+#else
+        public const bool DEBUG_MODE = false;
+#endif
+
+        private static readonly Harmony m_harmony = new Harmony("aragami.plateup.mods.betterdiscordRichPresence");
+
+        protected override void Initialise()
+        {
+            LogWarning($"{MOD_GUID} v{MOD_VERSION} in use!");
+        }
+
+        protected override void OnUpdate()
+        {
+        }
+
+        public void PostActivate(Mod _mod)
+        {
+            // Patch after initializing mods
+            LogWarning($"{MOD_GUID} v{MOD_VERSION} in use!");
+        }
+        #region Logging
+        public static void LogInfo(string _log) { Debug.Log($"[{MOD_NAME}] " + _log); }
+        public static void LogWarning(string _log) { Debug.LogWarning($"[{MOD_NAME}] " + _log); }
+        public static void LogError(string _log) { Debug.LogError($"[{MOD_NAME}] " + _log); }
+        public static void LogInfo(object _log) { LogInfo(_log.ToString()); }
+        public static void LogWarning(object _log) { LogWarning(_log.ToString()); }
+        public static void LogError(object _log) { LogError(_log.ToString()); }
+        #endregion
+    }
+}
