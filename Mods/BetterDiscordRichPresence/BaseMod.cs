09d8e10f86a1cd57d714d95b98fc8c40a510444a
Aragami_ <Aragami@aragami.com>
2024-01-20 15:48:11 +0100

    BDRP: Working, but not updating once connecting to other player

--- Mods/BetterDiscordRichPresence/BaseMod.cs
+++ Mods/BetterDiscordRichPresence/BaseMod.cs
@@ -1,12 +1,158 @@
-﻿using HarmonyLib;
+﻿using Discord;
+using HarmonyLib;
 using Kitchen;
+using Kitchen.NetworkSupport;
 using KitchenMods;
+using KitchenPrepUiDuringDay;
 using System.Reflection;
 using UnityEngine;
+using KitchenBetterDiscordRichPresence.Utility;
+using Unity.Entities;
+using System;
+using Steamworks;
+using TwitchLib.Api.Core.Models.Undocumented.ChannelPanels;
 
 // Namespace should have "Kitchen" in the beginning
 namespace KitchenBetterDiscordRichPresence
 {
+    [HarmonyPatch(typeof(SteamRichPresenceView), "UpdateDiscordRichPresence")]
+    public class Patch_Pre_SRPV
+    {
+        public static bool Prefix(SteamRichPresenceView.ViewData view_data, DiscordPlatform __instance)
+        {
+            BaseMod.LogInfo("WhyNotyHere?2");
+            if (DiscordPlatform.Discord.State != PlatformState.Ready)
+                return false;
+            string details = "";
+            if (view_data.Data.IsInGame)
+            {
+                if (view_data.Data.Day > 15)
+                {
+                    details = $"Overtime (Day {view_data.Data.Day - 15})";
+                }
+                else
+                {
+                    details = $"Restauranting (Day {view_data.Data.Day})";
+                }
+            }
+            else
+            {
+                details = "Planning";
+            }
+            string state = "";
+            if (view_data.Data.IsMultiplayer)
+            {
+                string str;
+                switch (view_data.Data.Players)
+                {
+                    case 1:
+                        str = "\uD83D\uDC69\u200D\uD83C\uDF73⚫⚫⚫";
+                        break;
+                    case 2:
+                        str = "\uD83D\uDC69\u200D\uD83C\uDF73\uD83D\uDC69\u200D\uD83C\uDF73⚫⚫";
+                        break;
+                    case 3:
+                        str = "\uD83D\uDC69\u200D\uD83C\uDF73\uD83D\uDC69\u200D\uD83C\uDF73\uD83D\uDC69\u200D\uD83C\uDF73⚫";
+                        break;
+                    case 4:
+                        str = "\uD83D\uDC69\u200D\uD83C\uDF73\uD83D\uDC69\u200D\uD83C\uDF73\uD83D\uDC69\u200D\uD83C\uDF73\uD83D\uDC69\u200D\uD83C\uDF73";
+                        break;
+                    case 5:
+                    case 6:
+                    case 7:
+                    case 8:
+                    case 9:
+                    case 10:
+                    case 11:
+                    case 12:
+                        str = String.Format($"{view_data.Data.Players}/\uD83D\uDC69\u200D\uD83C\uDF73");
+                        break;
+                    default:
+                        str = "";
+                        break;
+                }
+                state = str;
+            }
+            DiscordPlatform.Discord.SetActivity(state, details, view_data.Data.Players);
+            return false;
+        }
+    }
+
+    [HarmonyPatch(typeof(DiscordPlatform), nameof(DiscordPlatform.SetActivity))]
+    public class Patch_Pre_DPSA
+    {
+        public static bool Prefix(string state, string details, int players, DiscordPlatform __instance)
+        {
+            BaseMod.LogInfo("WhyNotyHere?1");
+            if (!__instance.IsReady)
+            {
+                return false;
+            }
+
+            EntityManager entityManager = World.DefaultGameObjectInjectionWorld.EntityManager;
+            Assembly mmoKitchen = Utility.Utility.GetLoadedAssembly("MMOKitchen.dll");
+
+            if (players == 0)
+            {
+                players = 1;
+            }
+
+            #region Time
+            EntityQuery entityQuery = entityManager.CreateEntityQuery(typeof(SSpeedrunDuration));
+            Entity my = entityQuery.GetSingletonEntity();
+            SSpeedrunDuration myCom = entityManager.GetComponentData<SSpeedrunDuration>(my);
+            long startTimeStamp = DateTimeOffset.UtcNow.ToUnixTimeSeconds() - (long)myCom.Seconds;
+            #endregion
+
+            Activity activity = new Activity
+            {
+                State = state,
+                Details = details,
+                Assets = new ActivityAssets
+                {
+                    LargeImage = "logosquare",
+                    LargeText = "PlateUp!",
+                    SmallImage = "logosquare",
+                    SmallText = "PlateUp!"
+                },
+                Type = ActivityType.Playing,
+                Timestamps = new ActivityTimestamps
+                {
+                    Start = startTimeStamp
+                }
+            };
+            if (__instance.Permissions != NetworkPermissions.Private)
+            {
+                int maxSize = 4;
+                if (mmoKitchen != null)
+                {
+                    maxSize = 12;
+                }
+                try
+                {
+                    activity.Secrets = new ActivitySecrets
+                    {
+                        Join = __instance.DiscordSDK.GetLobbyManager().GetLobbyActivitySecret(__instance.CurrentInviteLobby.Id)
+                    };
+                    activity.Party = new ActivityParty
+                    {
+                        Id = string.Format("{0}", __instance.CurrentInviteLobby.Id),
+                        Size = new PartySize
+                        {
+                            MaxSize = maxSize,
+                            CurrentSize = players
+                        }
+                    };
+                }
+                catch (ResultException)
+                {
+                }
+            }
+            __instance.DiscordSDK.GetActivityManager().UpdateActivity(activity, (ActivityManager.UpdateActivityHandler)(res => { }));
+            return false; // Don't run original
+        }
+    }
+
     public class BaseMod : GenericSystemBase, IModSystem
     {
         // GUID must be unique and is recommended to be in reverse domain name notation
@@ -33,6 +179,8 @@ namespace KitchenBetterDiscordRichPresence
         protected override void Initialise()
         {
             LogWarning($"{MOD_GUID} v{MOD_VERSION} in use!");
+            m_harmony.PatchAll(Assembly.GetExecutingAssembly());
+            LogWarning($"Patched");
         }
 
         protected override void OnUpdate()
@@ -42,7 +190,7 @@ namespace KitchenBetterDiscordRichPresence
         public void PostActivate(Mod _mod)
         {
             // Patch after initializing mods
-            LogWarning($"{MOD_GUID} v{MOD_VERSION} in use!");
+            LogWarning($"{MOD_GUID} v{MOD_VERSION} in use!1");
         }
         #region Logging
         public static void LogInfo(string _log) { Debug.Log($"[{MOD_NAME}] " + _log); }
---
+++ Mods/BetterDiscordRichPresence/Utility.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KitchenBetterDiscordRichPresence.Utility
+{
+    public static class Utility
+    {
+        public static System.Reflection.Assembly GetLoadedAssembly(string Name)
+        {
+            try
+            {
+                foreach (Assembly TempAssembly in AppDomain.CurrentDomain.GetAssemblies())
+                {
+                    string assemblyName = TempAssembly.GetName().Name;
+                    if (assemblyName.Equals(Name, StringComparison.InvariantCultureIgnoreCase)
+                        || assemblyName.Equals(Name + "-Workshop", StringComparison.InvariantCultureIgnoreCase)
+                    )
+                    {
+                        return TempAssembly;
+                    }
+                }
+                return null;
+            }
+            catch { return null; }
+        }
+    }
+}
