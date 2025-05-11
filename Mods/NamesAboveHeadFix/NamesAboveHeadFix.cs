442c055cb2f3bbaf1bf05b1e84b06d9da212fb82
Aragami_ <Aragami@aragami.com>
2025-05-11 20:02:42 +0200

    NamesAboveHeadFix

---
+++ Mods/NamesAboveHeadFix/NamesAboveHeadFix.cs
@@ -0,0 +1,53 @@
+﻿using HarmonyLib;
+using Kitchen;
+using KitchenMods;
+using System.Reflection;
+using UnityEngine;
+
+// Namespace should have "Kitchen" in the beginning
+namespace KitchenNamesAboveHead
+{
+    public class NamesAboveHeadFix : GenericSystemBase, IModSystem, IModInitializer
+    {
+        // GUID must be unique and is recommended to be in reverse domain name notation
+        // Mod Name is displayed to the player and listed in the mods menu
+        // Mod Version must follow semver notation e.g. "1.2.3"
+        public const string MOD_GUID = "aragami.plateup.mods.namesabovehead";
+        public const string MOD_NAME = "Names above head fix";
+        public const string MOD_VERSION = "0.1.0";
+        public const string MOD_AUTHOR = "Aragami";
+        public const string MOD_GAMEVERSION = ">=1.1.4";
+        // Game version this mod is designed for in semver
+        // e.g. ">=1.1.3" current and all future
+        // e.g. ">=1.1.3 <=1.2.3" for all from/until
+
+        // Boolean constant whose value depends on whether you built with DEBUG or RELEASE mode, useful for testing
+#if DEBUG
+        public const bool DEBUG_MODE = true;
+#else
+        public const bool DEBUG_MODE = false;
+#endif
+
+        private static readonly Harmony m_harmony = new(MOD_GUID);
+
+        public void PostActivate(KitchenMods.Mod mod)
+        {
+            // Early patch to patch InputSource before the first device connects
+            m_harmony.PatchAll(Assembly.GetExecutingAssembly());
+
+            LogWarning($"{MOD_GUID} v{MOD_VERSION} in useeee!");
+        }
+        #region Logging
+        public static void LogInfo(string _log) { Debug.Log($"[{MOD_NAME}] " + _log); }
+        public static void LogWarning(string _log) { Debug.LogWarning($"[{MOD_NAME}] " + _log); }
+        public static void LogError(string _log) { Debug.LogError($"[{MOD_NAME}] " + _log); }
+        public static void LogInfo(object _log) { LogInfo(_log.ToString()); }
+        public static void LogWarning(object _log) { LogWarning(_log.ToString()); }
+        public static void LogError(object _log) { LogError(_log.ToString()); }
+        #endregion
+
+        public void PreInject() { }
+        public void PostInject() { }
+        protected override void OnUpdate() { }
+    }
+}
---
+++ Mods/NamesAboveHeadFix/PlayerView_Patch.cs
@@ -0,0 +1,35 @@
+﻿using ExitGames.Client.Photon.StructWrapping;
+using HarmonyLib;
+using Kitchen;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TMPro;
+
+namespace KitchenNamesAboveHead
+{
+    [HarmonyPatch(typeof(PlayerIdentificationComponent))]
+    public class PlayerIdentificationComponent_Patch
+    {
+        [HarmonyPatch("Update")]
+        [HarmonyPrefix]
+        public static bool Update_PrefixPatch(PlayerIdentificationComponent __instance)
+        {
+            Traverse.Create(__instance).Field("UseNameLabel").SetValue(true);
+            return true;
+        }
+
+        [HarmonyPatch("Update")]
+        [HarmonyPostfix]
+        public static void Update_PostfixPatch(PlayerIdentificationComponent __instance)
+        {
+            Traverse traverse = Traverse.Create(__instance);
+            TextMeshPro textMeshPro = traverse.Field("NameLabel").GetValue<TextMeshPro>();
+            int playerid = traverse.Field("PlayerID").GetValue<int>();
+            textMeshPro.text = Players.Main.Get(playerid).Name;
+            traverse.Field("NameLabel").SetValue(textMeshPro);
+        }
+    }
+}
