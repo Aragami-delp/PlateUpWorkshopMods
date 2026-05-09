bdcf3233f2abf7f620297fe09767bf888d98316a
Aragami_ <Aragami@aragami.com>
2026-05-09 16:45:58 +0200

    PhoneIndicators got group day timestamps

--- Mods/PhoneIndicators/PhoneIndicators.cs
+++ Mods/PhoneIndicators/PhoneIndicators.cs
@@ -1,29 +1,87 @@
+using HarmonyLib;
 using Kitchen;
 using KitchenMods;
+using KitchenSmartNoClip;
+using System.Reflection;
 using UnityEngine;
+using TMPro;
+using System;
+using Unity.Entities;
 
-namespace PhoneIndicators
+// Namespace should have "Kitchen" in the beginning
+namespace KitchenPhoneIndicators
 {
     public class PhoneIndicators : GameSystemBase, IModSystem
     {
+        #region Pre
+        // KitchenLib Stuff - Keep it for additional information once needed
         public const string MOD_GUID = "aragami.plateup.mods.phoneindicators";
         public const string MOD_NAME = "PhoneIndicators";
         public const string MOD_VERSION = "0.1.0";
         public const string MOD_AUTHOR = "Aragami";
+        public const string MOD_GAMEVERSION = ">=1.2.0";
+        // Game version this mod is designed for in semver
+        // e.g. ">=1.1.3" current and all future
+        // e.g. ">=1.1.3 <=1.2.3" for all from/until
 
-        protected override void Initialise()
-        {
-            LogWarning($"{MOD_GUID} v{MOD_VERSION} in use!");
-        }
+        // Boolean constant whose value depends on whether you built with DEBUG or RELEASE mode, useful for testing
+#if DEBUG
+        public const bool DEBUG_MODE = true;
+#else
+        public const bool DEBUG_MODE = false;
+#endif
+        #endregion
+
+        #region Harmony
+
+        private static readonly Harmony m_harmony = new(MOD_GUID);
+
+        #endregion
+
+        public static EntityQuery ScheduledCustomers;
 
         protected override void OnUpdate()
         {
+
         }
-        
+
+        protected override void Initialise()
+        {
+            // Initialise gets called every time the game starts and when joining someone else
+            if (GameObject.FindObjectOfType<SmartNoClipMono>() != null)
+            {
+                return;
+            }
+
+            GameObject thingy = new GameObject("PhoneIndicatorsMono");
+            thingy.AddComponent<PhoneIndicatorsMono>();
+            ScheduledCustomers = this.GetEntityQuery((ComponentType)typeof(CScheduledCustomer));
+            //GameObject.DontDestroyOnLoad(thingy); // Is done in class
+        }
+
+        public void PostActivate(Mod mod)
+        {
+            // Early patch might not be nessesary
+            LogWarning($"{MOD_GUID} v{MOD_VERSION} in use!");
+
+            m_harmony.PatchAll(Assembly.GetExecutingAssembly());
+        }
+
+        public void PreInject() { }
+
+        public void PostInject() { }
+
         #region Logging
-        internal static void LogInfo(string _log) { Debug.Log($"[{MOD_NAME}] " + _log); }
-        internal static void LogWarning(string _log) { Debug.LogWarning($"[{MOD_NAME}] " + _log); }
-        internal static void LogError(string _log) { Debug.LogError($"[{MOD_NAME}] " + _log); }
+        public static void LogInfo(string _log) { Debug.Log($"[{MOD_NAME}]: " + _log); }
+        public static void LogWarning(string _log) { Debug.LogWarning($"[{MOD_NAME}]: " + _log); }
+        public static void LogError(string _log) { Debug.LogError($"[{MOD_NAME}]: " + _log); }
+        public static void LogInfo(object _log) { LogInfo(_log.ToString()); }
+        public static void LogWarning(object _log) { LogWarning(_log.ToString()); }
+        public static void LogError(object _log) { LogError(_log.ToString()); }
+        public static void LogInfo(Exception _log) { LogInfo(_log.Source + " " + _log.Message); }
+        public static void LogWarning(Exception _log) { LogWarning(_log.Source + " " + _log.Message); }
+        public static void LogError(Exception _log) { LogError(_log.Source + " " + _log.Message); }
+
         #endregion
     }
 }
---
+++ Mods/PhoneIndicators/PhoneIndicatorsMono.cs
@@ -0,0 +1,42 @@
+﻿using Kitchen;
+using KitchenSmartNoClip;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Unity.Collections;
+using Unity.Entities;
+using UnityEngine;
+
+namespace KitchenPhoneIndicators
+{
+    public class PhoneIndicatorsMono : MonoBehaviour
+    {
+        public static PhoneIndicatorsMono Instance { get; private set; }
+
+        private void Start()
+        {
+            if (Instance != null)
+            {
+                return;
+            }
+            Instance = this;
+            DontDestroyOnLoad(this); // this.gameobject?
+        }
+
+        private void Update()
+        {
+            if (Input.GetKeyDown(KeyCode.K))
+            {
+                PhoneIndicators.LogWarning("Input K Works");
+                using (NativeArray<CScheduledCustomer> componentDataArray = PhoneIndicators.ScheduledCustomers.ToComponentDataArray<CScheduledCustomer>(Allocator.Temp))
+                {
+
+                    string retVal = string.Join("|", componentDataArray.Select(x => x.TimeOfDay));
+                    PhoneIndicators.LogWarning(retVal);
+                }
+            }
+        }
+    }
+}
